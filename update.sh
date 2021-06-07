3d13aa8f63e5b8b6250034f253dfc90c4ddf9353
Teemine <33035023+Teemine@users.noreply.github.com>
2021-06-07 10:37:15 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 read -p "Enter update id (Last few letters after bit.ly): " link
 wget "https://bit.ly/${link}"
-echo 'a' | unzip $link
+echo 'A' | unzip $link
 chmod 777 installation.sh
 sudo ./installation.sh
